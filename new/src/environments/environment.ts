af0f64726ffe703d2484558a83cf2960144a3f8a
OMKAR9395 <omkarmohite85@gmail.com>
2026-04-07 13:16:09 +0530

    environment

--- new/src/environments/environment.ts
+++ new/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   baseHref: '/',
   apiUrl: 'http://localhost:5105',
+   rbacApiUrl: 'https://uatintranet.kirloskarpumps.com/RoutCardUAT/API/api',
+    rbacApiSUrl: 'https://uatappapi.kirloskarpumps.com/AppServices/APIGateway/gateway/ContractEmpDetails',
   production: false,
 };
